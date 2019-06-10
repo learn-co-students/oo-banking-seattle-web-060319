b8891356e90e90d48bba6062ecd079d98bdc18fc
Dex Fitch <fitch.dexter@gmail.com>
2019-06-09 21:25:14 -0700

    30793079

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,37 @@
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status
 
+  @@all = []
+
+  def initialize(name, balance = 1000, status = "open")
+    @name = name
+    @balance = balance
+    @status = status
+    @@all << self
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @balance > 0 && status == "open"
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
+  def self.all
+    @@all
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,41 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver
+  attr_accessor :amount, :status
+
+  @@all = []
+  
+  def initialize(sender, receiver, amount, status = "pending")
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = status
+    @@all << self
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+  end
+
+  def execute_transaction
+    if @status == "pending" && @sender.balance >= @amount
+      receiver.deposit(@amount)
+      sender.deposit(@amount * -1)
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      sender.deposit(@amount)
+      receiver.deposit(@amount * -1)
+      self.status = "reversed"
+    end
+  end
+
+  def self.all
+    @@all
+  end
 end
